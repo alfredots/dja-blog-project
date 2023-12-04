c6101c7b685512788dea69bd84b2077ceca1aaf3
Alfredo Tito <tito.alfred@hotmail.com>
2023-12-04 17:39:42 -0300

    feat: add content layer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import { allPosts } from 'contentlayer/generated';
+
 export default function Home() {
+  const posts = allPosts.map((post) => post);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h2>DJA NEXT TEMPLATE</h2>
+      <div dangerouslySetInnerHTML={{ __html: posts[0].title }} />
     </main>
   );
 }
