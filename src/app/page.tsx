8af1aa11a1c5e544c32b7985a71a9862326f2a5a
Alfredo Tito <tito.alfred@hotmail.com>
2023-12-10 16:51:02 -0300

    feat: mdx component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,35 @@
+import { Mdx } from '@/components/Mdx';
 import { allPosts } from 'contentlayer/generated';
+import tw from 'tailwind-styled-components';
+
+const Button = tw.button<{ $primary?: boolean }>`
+    ${(p) => (p.$primary ? 'bg-indigo-600' : 'bg-indigo-300')}
+
+    flex
+    inline-flex
+    items-center
+    border
+    border-transparent
+    text-xs
+    font-medium
+    rounded
+    shadow-sm
+    text-white
+
+    hover:bg-indigo-700
+    focus:outline-none
+`;
 
 export default function Home() {
   const posts = allPosts.map((post) => post);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen  flex-col items-center justify-between p-24">
       <h2>DJA NEXT TEMPLATE</h2>
       <div dangerouslySetInnerHTML={{ __html: posts[0].title }} />
+      <Button>mololow</Button>
+
+      <Mdx code={posts[0].body.code} />
     </main>
   );
 }
