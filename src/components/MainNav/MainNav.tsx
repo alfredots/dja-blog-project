92049cee995394d853af5d937076c4c01fc66d11
Alfredo Tito <tito.alfred@hotmail.com>
2023-12-30 17:53:28 -0300

    feat: make main nav responsive

--- src/components/MainNav/MainNav.tsx
+++ src/components/MainNav/MainNav.tsx
@@ -32,6 +32,20 @@ export const MainNav = ({ items }: MainNavProps) => {
             handleToggleMenu={handleToggleMenu}
           />
         </S.Content>
+
+        {isOpenMenu && (
+          <div className="fixed left-0 top-0 z-30 min-h-screen w-full overflow-hidden bg-primary/80 pt-[4rem] lg:hidden">
+            <S.NavMobile>
+              <S.ListMobile>
+                {items.mainNav.map((item, index) => (
+                  <S.ListItemMobile key={item.title + index}>
+                    <Link href={item.href}>{item.title}</Link>
+                  </S.ListItemMobile>
+                ))}
+              </S.ListMobile>
+            </S.NavMobile>
+          </div>
+        )}
       </S.Nav>
     </>
   );
--- src/components/MainNav/styles.ts
+++ src/components/MainNav/styles.ts
@@ -7,8 +7,9 @@ export const Nav = tw.nav`
 `;
 
 export const Ul = tw.ul`
-  flex
+  hidden
   gap-4
+  lg:flex
 `;
 
 export const Li = tw.li`
@@ -25,4 +26,32 @@ export const Content = tw.div`
   duration-300
   hover:text-link
   lg:hidden
+`;
+
+export const NavMobile = tw.nav`
+  mx-auto
+  flex
+  max-w-[80%]
+  items-center
+  justify-center
+  rounded-2xl
+  border
+  border-gray-600
+  bg-secundary
+  py-3
+  shadow-2xl
+`;
+
+export const ListMobile = tw.ul`
+  w-full
+  flex-col
+  items-center
+  justify-center
+  px-4
+`;
+
+export const ListItemMobile = tw.li`
+  border-b
+  border-gray-50
+  py-3
 `;
